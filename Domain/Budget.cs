5dc7b03c90a9e3d88f434d751489d9c3d0bdf2ba
takigavin <gavinhorner0@gmail.com>
2023-10-11 17:30:17 -0500

    fixed

---
+++ Domain/Budget.cs
@@ -0,0 +1,9 @@
+namespace Domain
+{
+    public class Budget
+    {
+        public string Month { get; set; }
+        public double AmountBudgeted { get; set; }
+        public double AmountLeft { get; set; }
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,9 @@
+namespace Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
